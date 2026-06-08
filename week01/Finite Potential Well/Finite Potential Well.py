c0a6163500aa32375fb7a922724f96f95d960f39
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-06-08 19:25:19 +0330

    Update Finite Potential Well.py

--- week01/Finite Potential Well/Finite Potential Well.py
+++ week01/Finite Potential Well/Finite Potential Well.py
@@ -1,1 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+x = np.linspace(-10,10,1000)
+V = np.zeros_like(x)
+V[(x>-2)&(x<2)] = -3
+
+
+plt.plot(x,V ,"r")
+plt.xlabel ="x"
+plt.ylabel = "V(x)"
+plt.legend()
+plt.title =("Finite Potential Well")
+plt.grid()
+plt.show()

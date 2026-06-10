0a0049e8f30af9e71f7226ab3d1b9e32c0d78ac6
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-06-10 20:40:14 +0330

    Update and rename Wavefunctions inside a finite well.py to day05_Wavefunctions inside a finite well.py

--- week01/day05_Wavefunctions inside a finite well.py
+++ week01/day05_Wavefunctions inside a finite well.py
@@ -1,24 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# محور x
+
 x = np.linspace(-8, 8, 1000)
 
 # finite well
 V = np.zeros_like(x)
 V[(x > -2) & (x < 2)] = -5
 
-# ضرایب مختلف
 a = [-0.2, -0.05, -0.5]
 
 plt.figure(figsize=(8,5))
 
-# رسم هر سه تابع موج
 for i in range(3):
     psi = np.exp(a[i] * x**2)
     plt.plot(x, psi, label=f'ψ{i+1}, a={a[i]}')
 
-# رسم پتانسیل
+
 plt.plot(x, V/5, 'k--', linewidth=2, label='Potential')
 
 plt.xlabel('x')

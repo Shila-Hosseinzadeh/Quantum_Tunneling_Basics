ab9101a6af378abc325fb3388e8391896e8d6de5
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-06-21 19:32:30 +0330

    Update energy_spectrum.py

--- week02/day08_eigenvalues/energy_spectrum.py
+++ week02/day08_eigenvalues/energy_spectrum.py
@@ -1,1 +1,11 @@
+import matplotlib.pyplot as plt
 
+plt.scatter(range(len(eigenvalues)), eigenvalues)
+
+plt.xlabel("State Index")
+plt.ylabel("Energy")
+plt.title("Energy Spectrum")
+
+plt.grid(True)
+
+plt.show()

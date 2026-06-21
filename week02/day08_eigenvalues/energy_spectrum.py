7152304ad367e0e82add6c7d0379eaf5e8397451
Shila Hosseinzadeh <shila.hosseinzadeh@gmail.com>
2026-06-21 19:34:15 +0330

    Update energy_spectrum.py

--- week02/day08_eigenvalues/energy_spectrum.py
+++ week02/day08_eigenvalues/energy_spectrum.py
@@ -1,4 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+N = 5
+
+H = np.zeros((N,N))
+
+for i in range(N-1):
+    H[i,i+1] = -1
+    H[i+1,i] = -1
 
 plt.scatter(range(len(eigenvalues)), eigenvalues)
 
